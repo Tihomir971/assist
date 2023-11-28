32af121451a57f1b6a364108c34af53faac88a4e
Tihomir971 <72653027+Tihomir971@users.noreply.github.com>
2023-11-28 10:55:54 +0100

    cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,13 @@
 module.exports = {
 	plugins: {
+		/* 	"postcss-preset-env": {
+				minimumVendorImplementations: 2,
+				features: {
+					'nesting-rules': false,
+					'custom-media-queries': true,
+					'media-query-ranges': true
+				}
+			}, */
 		'tailwindcss/nesting': {},
 		tailwindcss: {},
 		/* '@csstools/postcss-oklab-function': { 'preserve': true }, */
--- src/routes/(app)/Header.svelte
+++ src/routes/(app)/Header.svelte
@@ -5,7 +5,7 @@
 <header class="flex items-center justify-between border-b border-surface-3 px-4">
 	<div><strong>KALISI</strong> Assistant</div>
 	<div>
-		<input type="search" placeholder="Search..." class="focus-visible:outline-red-500" />
+		<input type="search" placeholder="Search..." />
 	</div>
 	<div class="flex h-full items-center justify-end space-x-2">
 		<Avatar src="" class="h-12"></Avatar>
