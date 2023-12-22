c587c83167e25743091f14ff259463669d93db5c
Tihomir971 <tihomir.jev@outlook.com>
2023-12-22 14:12:09 +0100

    tree toolbar 2

--- src/lib/components/treeview/components/TreeView.svelte
+++ src/lib/components/treeview/components/TreeView.svelte
@@ -4,18 +4,18 @@
 	import { setCtx } from '../ctx';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
-	import { FolderEdit } from 'lucide-svelte';
+	import { FolderEdit, FolderMinus, FolderPlus, FolderRoot } from 'lucide-svelte';
 	//Comment
 	export let treeItems: TreeItem[];
 	export let forceVisible: Props['forceVisible'] = undefined;
 	export let defaultExpanded: Props['defaultExpanded'] = [];
-	export let expanded: Props['expanded'] = undefined;
+	//	export let expanded: Props['expanded'] = undefined;
 	export let onExpandedChange: Props['onExpandedChange'] = undefined;
 
 	const {
 		elements: { tree },
-		states: { selectedItem }
-	} = setCtx({ forceVisible, defaultExpanded, expanded, onExpandedChange });
+		states: { selectedItem, expanded }
+	} = setCtx({ forceVisible, defaultExpanded, onExpandedChange });
 
 	function editCategory(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
 		const activeCategory = $page.url.searchParams.get('cat');
@@ -27,10 +27,10 @@
 <div class="flex h-full flex-col bg-surface-2 text-text-1">
 	<div class="flex gap-2 p-2 *:size-8 *:p-0">
 		<button type="button" on:click={editCategory}><FolderEdit strokeWidth={1.5} /></button>
-		<button type="button">-</button>
-		<button type="button">+</button>
-		<button type="button">+</button>
-		<button type="button">+</button>
+		<button type="button"><FolderPlus strokeWidth={1.5} /></button>
+		<button type="button"><FolderMinus strokeWidth={1.5} /></button>
+		<button type="button" on:click={() => ($expanded = [])}><FolderRoot strokeWidth={1.5} /></button
+		>
 	</div>
 
 	<ul class="w-full flex-grow !list-none overflow-auto pb-4 pt-2" {...$tree}>
