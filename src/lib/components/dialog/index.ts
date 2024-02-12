21ebc0be0987267b351a19dd739c85b0a48b0880
Tihomir971 <tihomir.jev@outlook.com>
2024-02-12 11:27:38 +0100

    import from $lib/components/dialog

--- src/lib/components/dialog/index.ts
+++ src/lib/components/dialog/index.ts
@@ -1,6 +1,6 @@
 import Root from './components/Root.svelte';
 import Trigger from './components/Trigger.svelte';
-import Portalled from './components/Portalled.svelte';
+import Portalled from '$lib/components/dialog/components/Portalled.svelte';
 export { getCtx } from './ctx';
 
 export const Dialog = {
