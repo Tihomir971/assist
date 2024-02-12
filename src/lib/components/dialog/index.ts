8d4879efc676ac5e3ddddbd79084e2a8945eef7f
Tihomir971 <tihomir.jev@outlook.com>
2024-02-12 10:43:27 +0100

    pnpm up

--- src/lib/components/dialog/index.ts
+++ src/lib/components/dialog/index.ts
@@ -1,6 +1,3 @@
-/* export { default as Dialog } from './components/dialog.svelte';
-export { default as DialogTitle } from './components/title.svelte';
-export { default as DialogPortalled } from './components/portalled.svelte'; */
 import Root from './components/Root.svelte';
 import Trigger from './components/Trigger.svelte';
 import Portalled from './components/Portalled.svelte';
