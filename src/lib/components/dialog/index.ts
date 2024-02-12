c56eb2e128cd685b84ca1727b8929059fed92231
Tihomir971 <tihomir.jev@outlook.com>
2024-02-12 11:18:44 +0100

    enable import Portalled

--- src/lib/components/dialog/index.ts
+++ src/lib/components/dialog/index.ts
@@ -1,10 +1,10 @@
 import Root from './components/Root.svelte';
 import Trigger from './components/Trigger.svelte';
-//import Portalled from '$lib/components/dialog/components/Portalled.svelte';
+import Portalled from '$lib/components/dialog/components/Portalled.svelte';
 export { getCtx } from './ctx';
 
 export const Dialog = {
 	Root: Root,
-	Trigger: Trigger
-	//	Portalled: Portalled
+	Trigger: Trigger,
+	Portalled: Portalled
 };
