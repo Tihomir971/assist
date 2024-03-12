3a2aeacace244b105fb321c1269e232b1a3c5dca
Tihomir971 <tihomir.jev@outlook.com>
2024-03-12 18:22:46 +0100

    const response = await fetch(fetchURL);

--- src/lib/services/scraper/product-info.ts
+++ src/lib/services/scraper/product-info.ts
@@ -34,10 +34,10 @@ export const getProductInfo = async (supabase: SupabaseClient<Database>, product
 				for (const key in getWebPrice) {
 					//					// Check if the string contains the current key
 					if (fetchURL.includes(key)) {
-						//						const response = await fetch(fetchURL);
-						//						if (!response.ok) {
-						//							throw new Error(`Failed to fetch: ${response.status}`);
-						//						}
+						const response = await fetch(fetchURL);
+						if (!response.ok) {
+							throw new Error(`Failed to fetch: ${response.status}`);
+						}
 						//						const html = await response.text();
 						//						const { document } = parseHTML(html);
 						//
