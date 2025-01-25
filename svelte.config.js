1cdc0ce27f6cd089ea5706973ed4ee62d7580653
Tihomir971 <tihomir.jev@outlook.com>
2025-01-25 11:55:48 +0100

    feat: comment out configPath in platformProxy settings in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 				exclude: ['<all>']
 			},
 			platformProxy: {
-				configPath: 'wrangler.toml',
+				// configPath: 'wrangler.toml',
 				environment: undefined,
 				experimentalJsonConfig: false,
 				persist: false
