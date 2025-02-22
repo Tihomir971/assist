60b86595047da78490197431072e21a8cd3ee67f
Tihomir971 <tihomir.jev@outlook.com>
2025-02-22 19:53:42 +0100

    feat: update project configuration and dependencies; enhance ESLint setup and improve styling variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
 		sveltekit(),
+		tailwindcss(),
 		Icons({
 			compiler: 'svelte'
 		})
